7c1784f51097de51b08eda051b29cad5d8e1af75
J0J0HA <80523427+J0J0HA@users.noreply.github.com>
2022-03-03 17:47:26 +0100

    Updated (Moved also agent to _agent; __init__ is now only referer to .references variables)

--- wwshc/references.py
+++ wwshc/references.py
@@ -1,9 +1,11 @@
 from ._user import User
+from ._agent import Agent
 from ._class import Class
 from ._group import Group
 from ._task import Task
-from .utils.caching import cache
+from .utils.events import Events
+from .utils.caching import Cache, cache
 from .utils.extra import void as __void
 
-__void(User, Class, Group, Task, cache)
+__void(Agent, User, Class, Group, Task, Events, Cache, cache)
 del __void
